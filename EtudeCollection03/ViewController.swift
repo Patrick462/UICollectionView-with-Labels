6358937f644aa78a6d98fc27168851ba6df53709
Patrick Weigel <patrick462@aol.com>
2015-04-05 20:12:33 -0500

    Initial Commit

---
+++ EtudeCollection03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EtudeCollection03
+//
+//  Created by Patrick Weigel on 4/5/15.
+//  Copyright (c) 2015 Patrick Weigel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
